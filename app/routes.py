dba887937a9a6c78e2cdb8033349c445a7a2a50a
tbrittain2 <tbrittain2@ucmerced.edu>
2022-08-21 22:20:24 -0400

    Routes.py, view_list, view_single changes
    Routes: added sample data to view_list and view_single changes
    View_list: Completed basic list(html and css)
    View_single: Completed grid outline w/ NO data, added jpg for page
    Base: Added sidebar

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,25 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sample_movies = [
+        {
+            'rank':1,
+            'ico':'ico',
+            'title':'Get Smart',
+            'genre':'Comedy',
+            'rating':'8.5/10',
+            'added_by':'Tyler'
+        },
+        {
+            'rank':2,
+            'ico':'ico',
+            'title':'Harry Potter and the Deathly HAllows',
+            'genre':'Fantasy',
+            'rating':'3/10',
+            'added_by':'Bryanna'
+        }
+    ]
+    return render_template('view_list.html', movies=sample_movies)
 
 
 @app.route('/search')
@@ -13,9 +31,25 @@ def search():
     return render_template('search.html')
 
 
-@app.route('/view')
-def view_single():
-    return render_template('view_single.html')
+@app.route('/view/<int:id>')
+def view_single(id=0):
+    if id == 0:
+        return '<h1> Error! </br> Invalid ID </h1>'
+    else:
+        sample = {
+            'id':1,
+            'icon':'harry_potter.jpg',
+            'title':'Harry Potter and the Deathly HAllows',
+            'release_year':2014,
+            'genre':'Fantasy',
+            'rating':'3/10',
+            'added_by':'Bryanna',
+            'director':'Joe Mama',
+            'actors':['danny boi', 'emma watson'],
+            'synopsis':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus dui urna, et iaculis odio ultrices et. Integer vel euismod sem. Etiam sed sapien sit amet massa blandit sollicitudin. In hac habitasse platea dictumst. Mauris pulvinar aliquet mauris, id egestas ante faucibus ac. Pellentesque eget erat venenatis nisi ultricies feugiat. ',
+            'imdb':'tt0926084'
+        }
+    return render_template('view_single.html', movie=sample)
 
 
 @app.route('/add')
