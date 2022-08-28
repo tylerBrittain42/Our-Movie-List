9584f13edeb3ef8c691b2a760199ceb40378d385
tbrittain2 <tbrittain2@ucmerced.edu>
2022-08-27 22:14:16 -0400

    added db class and resources for db

---
+++ Notes/db_resources.md
@@ -0,0 +1,7 @@
+# Database resources
+
+Database class design
+https://hackersandslackers.com/psycopg2-postgres-python/
+
+Modular db idea?
+https://stackoverflow.com/questions/55523299/best-practices-for-persistent-database-connections-in-python-when-using-flask#comment108272464_55537278
---
+++ app/db/db.py
@@ -0,0 +1,47 @@
+import psycopg
+from dotenv import dotenv_values
+
+# uri = f'postgresql://{credentials['USER']}:{credentials['PASSWORD']}@{credentials['IP']}:{credentials['PORT']}/{credentials['DBNAME']}'
+
+# test_query = 'SELECT * FROM movie WHERE m_id=4'
+
+# with psycopg.connect(conninfo=uri) as conn:
+#     with conn.cursor() as cur:
+#         cur.execute(test_query)
+#         # a = cur.fetchone()
+#         # print(cur)
+
+
+class Database:
+
+    def __init__(self) -> None:
+        credentials = dotenv_values('.db_env')
+
+        self.user = credentials['USER']
+        self.password = credentials['PASSWORD']
+        self.host = credentials['HOST']
+        self.port = credentials['PORT']
+        self.dbname = credentials['DBNAME']
+
+        self.conn = None
+
+    def connect(self):
+        """Connect to DB if not already connected"""
+        if self.conn is None:
+            try:
+                self.conn = psycopg.connect(
+                    host = self.host,
+                    user = self.user,
+                    password = self.password,
+                    port = self.port,
+                    dbname = self.dbname
+                )
+            except psycopg.DatabaseError as e:
+                print(e)
+                raise e
+            finally:
+                print('Connection opened successfully')
+            
+
+a = Database()
+a.connect()
