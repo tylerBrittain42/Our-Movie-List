858b53cad6f4901985af11bae889b7e349a65649
tbrittain2 <tbrittain2@ucmerced.edu>
2022-09-06 20:00:52 -0400

    moved nonfunctional model folder; updated models.py with all tables; migrated db

--- app/models.py
+++ app/models.py
@@ -1,5 +1,17 @@
-from email.policy import default
 from app import db
+
+
+user_list = db.Table('user_list',
+    db.Column('u_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('l_id', db.Integer, db.ForeignKey('list.id'), primary_key=True)
+)
+
+
+movie_list = db.Table('movie_list',
+    db.Column('m_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True),
+    db.Column('l_id', db.Integer, db.ForeignKey('list.id'), primary_key=True)
+)
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +27,25 @@ class Movie(db.Model):
 
     def __repr__(self):
         return f'{self.title} ({self.year}) by {self.director}'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'{self.id}) {self.name}'
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(80), nullable=False)
+    movie_list = db.relationship('Movie', secondary=movie_list, lazy='subquery',
+        backref=db.backref('lists', lazy=True))
+    user_list = db.relationship('User', secondary=user_list, lazy='subquery',
+        backref=db.backref('lists', lazy=True))
+
+    def __repr__(self):
+        return f'{self.id}) {self.name}'
+
