80d6023f06073d418091e03f6266d1d824e59b66
tbrittain2 <tbrittain2@ucmerced.edu>
2022-09-06 23:35:19 -0400

    script to generate sample data for models

---
+++ db_scripts/sample_generator.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+
+def main():
+
+    uri = 'postgresql://tyler:pass@127.0.0.1:5432/oml2'
+
+    run = True
+
+    names = ["James", "Robert", "John", "Michael", "David", "William", "Richard", "Joseph"]
+    password = 'pass'
+
+    generate_movies()
+    generate_users(names, password)
+    generate_lists(len(names))
+
+
+
+def generate_movies():
+    data_frame = pd.read_csv('db_scripts/IMDB_Data.csv')
+    data_frame = data_frame.drop(columns=['Rank','Runtime','Votes','Revenue','Metascore'])
+    data_frame.to_csv('db_scripts/sample_movie.csv', index=False)
+
+
+def generate_users(names,password):
+    with open('db_scripts/sample_users.csv', 'w') as f:
+        f.write('name,password\n')
+        for name in names:
+            f.write(f'{name},{password}\n')
+
+
+def generate_lists(num):
+    with open('db_scripts/sample_lists.csv', 'w') as f:
+        f.write('name\n')
+        for ele in range(num):
+            f.write(f'{ele}\n')
+
+
+
+
+if __name__ == '__main__':
+    main()
