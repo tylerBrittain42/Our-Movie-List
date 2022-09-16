adc94c6c249368b1c822a90d5faf5888df8441af
tbrittain2 <tbrittain2@ucmerced.edu>
2022-09-16 00:16:44 -0400

    restructured init folder w/ only hello-world capabilities

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,35 @@
-from config import Config
+# from config import Config
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app,db)
+
+# from app import routes, models
+
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    
+    if test_config is None:
+        app.config.from_object(Config)
+    else:
+        app.config.from_mapping(test_config)
 
-from app import routes, models
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        print('error')
+        pass
+
+    @app.route('/')
+    def init_route():
+        return 'it works'
+
+    return app
