e8f046688ee9a5877b5be109b397699532bb9ba3
tbrittain2 <tbrittain2@ucmerced.edu>
2022-09-22 00:49:32 -0400

    updated requirements.txt; added barebones open/close db connection functionality

--- oml/db.py
+++ oml/db.py
@@ -3,6 +3,21 @@ import psycopg
 import click
 from flask import current_app, g
 
+
 def get_db():
+    
+    # REMOVE THIS 
+    uri = 
+    # REMOVE THIS
+
     if 'db' not in g:
-        g.db = = 'a'
+        g.db  = psycopg.connect(uri) # optionally, we could configure the row factory type here
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
