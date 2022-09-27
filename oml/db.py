99218e1e7c04ee5a0294e868b7a03c7e6fcd4f73
tbrittain2 <tbrittain2@ucmerced.edu>
2022-09-27 01:16:52 -0400

    import uri to db.py(from .env)

--- oml/db.py
+++ oml/db.py
@@ -3,15 +3,19 @@ import psycopg
 import click
 from flask import current_app, g
 
+# replace me with config file
+from dotenv import dotenv_values
+
+env_vals = dotenv_values('.env')
 
 def get_db():
     
     # REMOVE THIS 
-    uri = 
-    # REMOVE THIS
+    URI = f'{env_vals["DB_TYPE"]}://{env_vals["USER"]}:{env_vals["PASSWORD"]}@' + \
+        f'{env_vals["HOST"]}:{env_vals["PORT"]}/{env_vals["DBNAME"]}'    # REMOVE THIS
 
     if 'db' not in g:
-        g.db  = psycopg.connect(uri) # optionally, we could configure the row factory type here
+        g.db  = psycopg.connect(URI) # optionally, we could configure the row factory type here
     return g.db
 
 
@@ -20,4 +24,3 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
-
