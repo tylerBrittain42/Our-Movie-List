dfc95ce01687ac83c80c0a79aadd7b6d68d44f31
tbrittain2 <tbrittain2@ucmerced.edu>
2022-10-04 23:50:08 -0400

    implement login_required func

--- oml/auth.py
+++ oml/auth.py
@@ -91,3 +91,14 @@ def login_get():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login_get'))
+
+        return view(**kwargs)
+
+    return wrapped_view
