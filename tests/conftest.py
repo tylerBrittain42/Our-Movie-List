3081e573ec812487faceccce4deb92d2136fa383
tbrittain2 <tbrittain2@ucmerced.edu>
2022-10-12 23:42:31 -0400

    implement testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
 import os
 import tempfile
-
 import pytest
-from flaskr import create_app
-from flaskr.db import get_db, init_db
+from oml import create_app
+from oml.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
@@ -36,3 +35,21 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
