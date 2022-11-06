ceeebc41d57df5e107a7f45c42475e5a1e034361
tbrittain2 <tbrittain2@ucmerced.edu>
2022-11-05 20:39:11 -0400

    implement pagination for /all/

--- oml/blueprints/movie/__init__.py
+++ oml/blueprints/movie/__init__.py
@@ -15,8 +15,23 @@ def indiv_movie(movie_id):
     
     # getting movie info
     db = get_db()
-    movie_data = db.execute('SELECT * FROM movie WHERE id = (%s)', (movie_id,)).fetchone()
+    movie_data = db.execute('SELECT * FROM movie WHERE id = (%s);', (movie_id,)).fetchone()
     if movie_data is not None:
         return render_template('movie/view_single.html', movie=movie_data, poster='harry_potter.jpg')
     else:
         return '<h1> INVALID ID </h1>'
+
+@bp.get('/all/<page>')
+@login_required
+def browse_all(page):
+
+    offset_value = int(page) * 10
+    PAGE_SIZE = 10
+
+    db = get_db()
+    movie_list = db.execute('''SELECT id, title, year FROM movie 
+                               ORDER BY id 
+                               ASC OFFSET (%s) 
+                               LIMIT (%s);''', (offset_value, PAGE_SIZE)).fetchall()
+
+    return render_template('view_all', movie_list=movie_list, poster='harry_potter.jpg')
