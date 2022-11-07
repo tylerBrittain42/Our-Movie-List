897ad2ef0f7178bc9263470dcdf59d55f4f5e0c3
tbrittain2 <tbrittain2@ucmerced.edu>
2022-11-07 00:40:38 -0500

    add temp navbar to base; add "page x of x footer"

--- oml/__init__.py
+++ oml/__init__.py
@@ -16,6 +16,7 @@ from config import Config
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.url_map.strict_slashes = False # REMOVE ME LATER
     
     if test_config is None:
         app.config.from_object(Config)
